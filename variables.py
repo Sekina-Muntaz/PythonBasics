25c24c22d8e8f240c3cd59153f03ce5bba377512
Sekina-Muntaz <sherzmuntaz@gmail.com>
2020-03-10 17:19:22 +0300

    variables, identifiers and keywords

---
+++ variables.py
@@ -0,0 +1,24 @@
+firstName="Sekina"
+lastName="Muntaz"
+favNum=10
+student=True
+employed=False
+
+print(type(firstName))
+#print(lastName)
+print(type(favNum))
+print(type(student))
+# print(employed)
+
+school="TechCamp"
+print(school[3])
+print(school[3])
+print(school[4:8])
+print(school[4:])
+print(school[-4:])
+
+
+
+
+
+
