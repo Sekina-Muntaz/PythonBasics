3964836059596415beac4fe883a78138e505e4a3
Sekina-Muntaz <sherzmuntaz@gmail.com>
2020-03-11 18:04:19 +0300

    data structures and modification

--- hello2.py
+++ hello2.py
@@ -1,1 +1,18 @@
+# displays Sekina on screen
+# print("Sekina")
 print("Sekina")
+print("Sekina")
+print("Sekina")
+print("Sekina")
+print("Sekina")
+
+
+# triple quotes can be used for multiline strings
+name='''James
+jane'''
+
+# triple  double quotes can be used for multiline strings
+Name="""George
+Tom"""
+
+print(name)
