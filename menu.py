dd214e9a7a1d9bbb378c7ecc70f3f927357ad804
Sekina-Muntaz <sherzmuntaz@gmail.com>
2020-03-13 18:06:57 +0300

    iterations and loops for loop

--- menu.py
+++ menu.py
@@ -3,15 +3,42 @@ print("2. 30MB")
 print("3. 40MB")
 
 choice=int(input("enter selection: "))
-print(type(choice))
+# print(type(choice))
+balance=100
 
-if choice==1:
-    print("You have bought 20MB")
-elif choice==2:
-    print("You have bought 30MB")
+if choice==1:#and balance>=20:
+    if balance>=20:
+        balance=balance-20
+        print("You have received 20MBs")
+        print("New Balance ",balance)
+
+
+    else:
+        print("Insufficient balance")
+
+elif choice==2:#and balance>=20:
+    if balance>=30:
+        balance=balance-30
+        print("You have received 30MBs")
+        print("New Balance ",balance)
+
+
+    else:
+        print("Insufficient balance")
+elif choice==3:
+    if balance>=40:
+        balance=balance-40
+        print("You have received 40MBs")
+        print("New Balance ", balance)
+    else:
+        print("insufficient balance")
+
+
+
+'''
 elif choice==3:
     print("You have bought 40MB")
 else:
-    print("invalid")
+    print("invalid")'''
 
 
--- orOperator.py
+++ orOperator.py
@@ -2,10 +2,12 @@
 
 tk1=input("enter ticket one: ")
 
-print(type(tk1))
+# print(type(tk1))
 
 tk2=int(tk1)
+
 print(type(tk2))
+
 if tk2==13 or tk2==7:
     print("enter party")
 else:
